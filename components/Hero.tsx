3fab31fda201dfc82523b915cccd8ff68c177f7e
Betül Sarı <betulsari_97@hotmail.com>
2023-10-19 13:09:01 +0300

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,15 +11,15 @@ const Hero = () => {
         <div className="hero">
             <div className="flex-1 pt-36 padding-x">
                 <h1 className="hero__title">
-                    Organize your weekly -schedules and programs easily!
+                    Find, book, or rent a car - quickly and easily!
                 </h1>
 
                 <p className="hero__subtitle">
-                    Create your weekly schedule online and reward yourself for what you can do
+                    Streamline your car rental experience with our effortless booking process.
                 </p>
 
                 <CustomButton 
-                    title= "Explore ready Programs"
+                    title= "Explore Cars"
                     containerStyles="bg-primary-blue text-white rounded-full mt-10"
                     handleClick={handleScroll}
                 />
@@ -27,8 +27,8 @@ const Hero = () => {
             <div className="hero__image-container">
                 <div className="hero__image">
                     <Image src="/hero.png" alt="hero" fill className="object-contain" />
-                    <div className="hero__image-overlay" />
                 </div>
+                <div className="hero__image-overlay" /> 
             </div>
         </div>
     )
